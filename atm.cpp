1ff364c54877b35db9cf8d216cdeda99c7677204
jim-nnamdi <jimsamuel50@gmail.com>
2022-08-12 22:42:30 +0100

    add menu and call show_menu in main function

--- atm.cpp
+++ atm.cpp
@@ -3,5 +3,16 @@
 using namespace std;
 
 void show_menu(){
-    
+    cout << "******** Menu ********" << endl;
+    cout << "1. Check balance" << endl ;
+    cout << "2. Deposit funds" << endl;
+    cout << "3. Withdraw funds" << endl;
+    cout << "4. Exit cli" << endl;
+    cout << "***********************" << endl;
+    system("cls");
+}
+
+int main(){
+    show_menu();
+    system("pause>0");
 }
