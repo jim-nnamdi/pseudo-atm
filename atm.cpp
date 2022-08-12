84a481d99dfd4863e42256c2bbf65d179aa09dd7
jim-nnamdi <jimsamuel50@gmail.com>
2022-08-12 22:57:58 +0100

    enclose logic in a do-while loop to prevent cli exit

--- atm.cpp
+++ atm.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include<stdlib.h>
+
 using namespace std;
 
 void show_menu(){
@@ -9,10 +11,33 @@ void show_menu(){
     cout << "3. Withdraw funds" << endl;
     cout << "4. Exit cli" << endl;
     cout << "***********************" << endl;
-    system("cls");
 }
 
 int main(){
     show_menu();
-    system("pause>0");
-}
+    double account_balance = 500;
+    int option;
+    do {
+    switch (option)
+    {
+    case 1:
+        cout << "The balance of the account is :" << account_balance << endl;
+        break;
+    case 2:
+        double deposit_amount; 
+        cout << "How much do you want to deposit" << endl;
+        cin >> deposit_amount;
+        account_balance += deposit_amount;
+        break;
+    case 3: 
+        double withdrawal_amount;
+        cout << "How much do you want to withdraw";
+        cin >> withdrawal_amount;
+        account_balance -= withdrawal_amount;
+        break;
+    default:
+        break;
+    }
+    return 0;
+} while(option != 4 || option >= 4);
+} 
